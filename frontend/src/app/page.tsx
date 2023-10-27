0f7d637fc99c23b2827bcc64cfe91dd7848f02f3
akocoglu <ahmetkocoglu@outlook.com>
2023-10-27 20:05:18 +0300

    login

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,32 +1,36 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { use, useEffect, useState } from "react";
 import axios from "axios";
 
 export default function Home() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [token, setToken] = useState("");
+  const [user, setUser] = useState<any>({});
 
   const handleLogin = () => {
     axios
       .post("http://localhost:3040/users/login", { email, password })
       .then((response: any) => {
-        console.log(response.data);
-        setToken(response.data)
+        setToken(response.data);
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  const handleMe = () => {
-    axios.defaults.baseURL = 'http://localhost:3040/';
-    axios.defaults.headers.common['Authorization'] = token;
+  useEffect(() => {
+    if (token !== "") me();
+  }, [token]);
+
+  const me = () => {
+    axios.defaults.baseURL = "http://localhost:3040/";
+    axios.defaults.headers.common["Authorization"] = token;
     axios
       .get("users/me")
       .then((response) => {
-        console.log(response.data);
+        setUser(response.data);
       })
       .catch((error) => {
         console.log(error);
@@ -34,25 +38,31 @@ export default function Home() {
   };
   return (
     <main className="flex min-h-screen flex-col items-center p-24 bg-white">
-      <input
-        type="text"
-        className="mb-1 text-black border border-gray-600 px-3 py-1"
-        onChange={(e: any) => setEmail(e.target.value)}
-        placeholder="email"
-      />
-      <input
-        type="password"
-        className="mb-1 text-black border border-gray-600 px-3 py-1"
-        onChange={(e: any) => setPassword(e.target.value)}
-        placeholder="password"
-      />
-      <button type="submit" onClick={handleLogin} className="text-black">
-        Login
-      </button>
-      <hr />
-      <button type="button" onClick={handleMe} className="text-black">
-        Doğrula
-      </button>
+      {token === "" ? (
+        <>
+          <input
+            type="text"
+            className="mb-1 text-black border border-gray-600 px-3 py-1"
+            onChange={(e: any) => setEmail(e.target.value)}
+            placeholder="email"
+          />
+          <input
+            type="password"
+            className="mb-1 text-black border border-gray-600 px-3 py-1"
+            onChange={(e: any) => setPassword(e.target.value)}
+            placeholder="password"
+          />
+          <button type="submit" onClick={handleLogin} className="text-black">
+            Login
+          </button>
+        </>
+      ) : (
+        <>
+          <div className="text-black">
+            {user.firstName} {user.lastName} {user.phone} {user.email}
+          </div>
+        </>
+      )}
     </main>
-  )
+  );
 }
